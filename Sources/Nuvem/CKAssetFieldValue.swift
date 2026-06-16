8f62e7e124e5a829a582d29c3b87ea2e168d28fc
Mateus Rodrigues <mateus.fr26@gmail.com>
2026-06-16 11:19:15 -0300

    Add NSImage, CIImage, CGImage conformances to CKAssetFieldValue

--- Sources/Nuvem/CKAssetFieldValue.swift
+++ Sources/Nuvem/CKAssetFieldValue.swift
@@ -47,4 +47,68 @@ extension UIImage: CKAssetFieldValue {
     
 }
 
+
 #endif
+
+#if canImport(AppKit)
+
+import AppKit
+
+extension NSImage: CKAssetFieldValue {
+    
+    public static func get(_ recordValue: Data) -> Self? {
+        return Self(data: recordValue)
+    }
+    
+    public static func set(_ value: NSImage?) -> Data? {
+        return value?.tiffRepresentation
+    }
+    
+}
+
+
+#endif
+
+extension CIImage: CKAssetFieldValue {
+    
+    public static func get(_ recordValue: Data) -> Self? {
+        return Self(data: recordValue)
+    }
+    
+    public static func set(_ value: CIImage?) -> Data? {
+        let context = CIContext()
+        guard let value else {
+            return nil
+        }
+        guard let colorSpace = CGColorSpace(name: CGColorSpace.sRGB) else {
+            return nil
+        }
+        return try? context.pngRepresentation(
+            of: value,
+            format: .RGBA8,
+            colorSpace: colorSpace,
+            options: [:]
+        )
+    }
+    
+}
+
+extension CGImage: CKAssetFieldValue {
+    
+    public static func get(_ recordValue: Data) -> Self? {
+        guard let ciImage = CIImage.get(recordValue) else {
+            return nil
+        }
+        let context = CIContext(options: nil)
+        return context.createCGImage(ciImage, from: ciImage.extent) as? Self
+    }
+    
+    public static func set(_ value: CGImage?) -> Data? {
+        guard let value else {
+            return nil
+        }
+        var ciImage = CIImage(cgImage: value)
+        return CIImage.set(ciImage)
+    }
+    
+}
